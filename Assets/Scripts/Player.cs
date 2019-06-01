6f76d357ea9d9ca3d7bae450737b3eb2dafd7fa1
KarowM <karow.foxx@gmail.com>
2019-06-01 23:13:10 +0100

    Clampted player to screen

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
 
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+
+    private float xMin, xMax;
+    private float yMin, yMax;
 
     void Start() {
+        SetupMoveBoundaries();
+    }
 
+    private void SetupMoveBoundaries() {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     // Update is called once per frame
@@ -19,8 +32,8 @@ public class Player : MonoBehaviour {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
     }
 }
