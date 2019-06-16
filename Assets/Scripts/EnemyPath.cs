1c171ff0e6a3570141c0f0f734aa704a523677a8
KarowM <karow.foxx@gmail.com>
2019-06-16 01:36:13 +0100

    Added enemy spawner

--- Assets/Scripts/EnemyPath.cs
+++ Assets/Scripts/EnemyPath.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 
 public class EnemyPath : MonoBehaviour {
 
-    [SerializeField] WaveConfig waveConfig;
+    WaveConfig waveConfig;
     [SerializeField] List<Transform> waypoints;
-    [SerializeField] float moveSpeed = 2f;
     int waypointIndex = 0;
 
     void Start() {
@@ -19,10 +18,14 @@ public class EnemyPath : MonoBehaviour {
         Move();
     }
 
+    public void SetWaveConfig(WaveConfig waveConfig) {
+        this.waveConfig = waveConfig;
+    }
+
     private void Move() {
         if (waypointIndex <= waypoints.Count - 1) {
             var targetPosition = waypoints[waypointIndex].transform.position;
-            var movementThisFrame = moveSpeed * Time.deltaTime;
+            var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
             if (transform.position == targetPosition) {
                 waypointIndex++;
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startingWave = 0;
+
+    void Start() {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig) {
+        for (int i = 0; i < waveConfig.GetNumberOfEnemies(); i++) {
+            var newEnemy = Instantiate(
+                waveConfig.GetEnemyPrefab(),
+                waveConfig.GetWaypoints()[0].transform.position,
+                Quaternion.identity);
+            newEnemy.GetComponent<EnemyPath>().SetWaveConfig(waveConfig);
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+}
