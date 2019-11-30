8a63bb77026f6e492c7dcb9be16a8866e9114d8f
KarowM <karow.foxx@gmail.com>
2019-11-30 19:43:20 +0000

    added enemy laser perfabs, enemy shoots at random intervals

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,10 +1,35 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    private readonly float projectileSpeed = 10f;
+
     [SerializeField] float health = 300f;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject laserPrefab;
+
+    private void Start() {
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
 
     void Update() {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot() {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f) {
+            Fire();
+            shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        } 
+    }
+
+    private void Fire() {
+        GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
 
     }
 
