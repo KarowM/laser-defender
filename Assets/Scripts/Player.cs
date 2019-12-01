a975cd5082a4e97eae948c0e391c61869a5571d4
KarowM <karow.foxx@gmail.com>
2019-12-01 22:15:03 +0000

    added layers for collissions, added null check for damage dealer on collisions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -32,11 +32,14 @@ public class Player : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D other) {
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
-        ProcessHit(damageDealer);
+        if (damageDealer) {
+            ProcessHit(damageDealer);
+        }
     }
 
     private void ProcessHit(DamageDealer damageDealer) {
         health -= damageDealer.GetDamage();
+        damageDealer.Hit();
         if (health <= 0) {
             Destroy(gameObject);
         }
