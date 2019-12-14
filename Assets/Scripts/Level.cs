a3a552f5d4ac5671a3cdea7a651d1ed06ae16ce7
KarowM <karow.foxx@gmail.com>
2019-12-14 18:45:23 +0000

    added start and game over scenes

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour {
+
+    public void LoadStartMenu() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame() {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadGameOver() {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
